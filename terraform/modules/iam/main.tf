e2960e1e57febcd7efa061ef3ce3f2af0460e63c
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-08-18 15:18:00 +1200

    updated iam policy setup

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,64 +1,146 @@
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-#====================================
-
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "cloudacademy-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name = "cloudacademy-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-data "aws_iam_policy" "amazon_ec2_container_service_for_ec2_role" {
-  name = "AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = data.aws_iam_policy.amazon_ec2_container_service_for_ec2_role.arn
-}
-
-resource "aws_iam_role_policy" "secretsmanager_policy" {
-  name = "password-policy-secretsmanager"
+resource "aws_iam_role_policy" "ecs_task_execution_policy" {
+  name = "cloudacademy-ecs-task-execution-policy"
   role = aws_iam_role.ecs_task_execution_role.id
 
   policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": [
-          "secretsmanager:GetSecretValue"
-        ],
-        "Effect": "Allow",
-        "Resource": [
-          "${var.secretsmanager_db_creds_arn}"
-        ]
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "${var.secretsmanager_db_creds_arn}"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeTags",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:UpdateContainerInstancesState",
+        "ecs:Submit*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "ecs:TagResource",
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "ecs:CreateAction": [
+            "CreateCluster",
+            "RegisterContainerInstance"
+          ]
+        }
       }
-    ]
-  }
-  EOF
+    }
+  ]
+}
+EOF
 }
 
 #====================================
 
 resource "aws_iam_role" "ecs_exec_task_role" {
-  name               = "cloudacademy-ecs-exec-task-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name = "cloudacademy-ecs-exec-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-data "aws_iam_policy" "amazon_ssm_managed_instance_core" {
-  name = "AmazonSSMManagedInstanceCore"
-}
+resource "aws_iam_role_policy" "ecs_exec_task_policy" {
+  name = "cloudacademy-ecs-exec-task-policy"
+  role = aws_iam_role.ecs_exec_task_role.id
 
-resource "aws_iam_role_policy_attachment" "ecs_ssm_role_policy_attach" {
-  role       = aws_iam_role.ecs_exec_task_role.name
-  policy_arn = data.aws_iam_policy.amazon_ssm_managed_instance_core.arn
+  policy = <<-EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:DescribeAssociation",
+        "ssm:GetDeployablePatchSnapshotForInstance",
+        "ssm:GetDocument",
+        "ssm:DescribeDocument",
+        "ssm:GetManifest",
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:ListAssociations",
+        "ssm:ListInstanceAssociations",
+        "ssm:PutInventory",
+        "ssm:PutComplianceItems",
+        "ssm:PutConfigurePackageResult",
+        "ssm:UpdateAssociationStatus",
+        "ssm:UpdateInstanceAssociationStatus",
+        "ssm:UpdateInstanceInformation"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2messages:AcknowledgeMessage",
+        "ec2messages:DeleteMessage",
+        "ec2messages:FailMessage",
+        "ec2messages:GetEndpoint",
+        "ec2messages:GetMessages",
+        "ec2messages:SendReply"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
