9cdbe5bb935abec92eb25858b386f7f995e7152d
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-08-18 16:36:28 +1200

    updated outputs - removed ecs role arn

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,10 +18,6 @@ output "public-alb-fqdn" {
   value = module.public_alb.dns
 }
 
-output "ecs-task-execution-role-arn" {
-  value = module.iam.ecs_task_execution_role_arn
-}
-
 output "web_app_wait_command" {
   value       = "until curl -Is --max-time 5 http://${module.public_alb.dns}/api/stocks/csv | grep 'HTTP/1.1 200'; do echo preparing...; sleep 5; done; echo; echo -e 'Ready...'"
   description = "Test command - tests readiness of the web app"
