7462a202af1bc11cf7ffc9df4c11b5eb6611a67b
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-09-12 09:31:10 +1200

    updated outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,6 +18,10 @@ output "public-alb-fqdn" {
   value = module.public_alb.dns
 }
 
+output "frontend-url" {
+  value = "http://${module.public_alb.dns}"
+}
+
 output "web_app_wait_command" {
   value       = "until curl -Is --max-time 5 http://${module.public_alb.dns}/api/stocks/csv | grep 'HTTP/1.1 200'; do echo preparing...; sleep 5; done; echo; echo -e 'Ready...'"
   description = "Test command - tests readiness of the web app"
